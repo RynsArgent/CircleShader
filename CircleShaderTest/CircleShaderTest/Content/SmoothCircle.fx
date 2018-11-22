cc5529ccd6d45e8afedb6f8e05080fae81570402
Calvin Phung <RynsArgent@hotmail.com>
2018-11-22 11:10:37 -0800

    CP Upload monogame circle shader files

---
+++ CircleShaderTest/CircleShaderTest/Content/SmoothCircle.fx
@@ -0,0 +1,25 @@
+﻿#if OPENGL
+	#define PS_SHADER ps_3_0
+#else
+	#define PS_SHADER ps_4_0_level_9_1
+#endif
+
+float4 PixelShaderFunction(float4 color : COLOR0, float2 uv : TEXCOORD0) : COLOR0
+{
+    // https://blogs.msdn.microsoft.com/shawnhar/2011/10/31/shader-aliasing/
+    float2 circleCenter = float2(0.5f, 0.5f);
+    float circleRadius = 0.5f;
+    float distanceFromCenter = length(circleCenter - uv);
+    
+    float alpha = (distanceFromCenter < circleRadius) ? 1 : 0;
+
+    return color * alpha;
+}
+
+technique Technique1
+{
+    pass Pass1
+    {
+        PixelShader = compile PS_SHADER PixelShaderFunction();
+    }
+}
---
+++ CircleShaderTest/CircleShaderTest/Game1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CircleShaderTest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D dummytex;
+        Effect myEffect;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1024;
+            graphics.PreferredBackBufferHeight = 768;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            dummytex = Content.Load<Texture2D>("dummytex");
+
+            myEffect = Content.Load<Effect>("SmoothCircle");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        int size;
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            size = (int)System.Math.Round(System.Math.Sin(MathHelper.PiOver4 * gameTime.TotalGameTime.TotalSeconds) * 256 + 256);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            Point center = new Point(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, myEffect, null);
+            spriteBatch.Draw(dummytex, new Rectangle(center.X - size / 2, center.Y - size / 2, size, size), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
